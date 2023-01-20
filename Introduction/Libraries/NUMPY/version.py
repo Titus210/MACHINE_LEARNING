8a60a24926f41bcf871be59a1684082d234fc430
Titus Kiplagat <tituskiplagat210@gmail.com>
2023-01-21 01:43:53 +0300

    Getting Numpy Version and configuration

---
+++ Introduction/Libraries/NUMPY/version.py
@@ -0,0 +1,8 @@
+# import Numpy Library
+import numpy as np
+
+version = np.__version__
+config = np.show_config()
+
+print(f"Numpy Version is \n{version}")
+print(f"Numpy configuration is \n{config}")
